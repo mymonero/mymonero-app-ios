77d45a0069723fc42710c60ff7b84ad513206305
Paul Shapiro <pshapiro138@gmail.com>
2017-06-22 20:16:32 -0500

    fixed up spacing in WalletsListViewCell

--- Modules/Wallets/Views/WalletsListViewCell.swift
+++ Modules/Wallets/Views/WalletsListViewCell.swift
@@ -11,7 +11,10 @@ import UIKit
 class WalletsListViewCell: UITableViewCell
 {
 	static let reuseIdentifier = "WalletsListViewCell"
-	static let cellHeight: CGFloat = 100
+	static let contentViewHeight: CGFloat = 80
+	static let contentView_margin_h: CGFloat = 16
+	static let cellSpacing: CGFloat = 12
+	static let cellHeight: CGFloat = contentViewHeight + cellSpacing
 	//
 	let cellContentView = WalletCellContentView(sizeClass: .large48)
 	let accessoryChevronView = UIImageView(image: UIImage(named: "list_rightside_chevron")!)
@@ -68,7 +71,15 @@ class WalletsListViewCell: UITableViewCell
 	override func layoutSubviews()
 	{
 		super.layoutSubviews()
-		let frame = UIEdgeInsetsInsetRect(self.bounds, UIEdgeInsetsMake(0, 16, 16, 16))
+		let frame = UIEdgeInsetsInsetRect(
+			self.bounds,
+			UIEdgeInsetsMake(
+				0,
+				WalletsListViewCell.contentView_margin_h - UICommonComponents.HighlightableCells.imagePaddingForShadow_h,
+				WalletsListViewCell.cellSpacing - 2*UICommonComponents.HighlightableCells.imagePaddingForShadow_v,
+				WalletsListViewCell.contentView_margin_h - UICommonComponents.HighlightableCells.imagePaddingForShadow_h
+			)
+		)
 		self.contentView.frame = frame
 		self.backgroundView!.frame = frame
 		self.selectedBackgroundView!.frame = frame
