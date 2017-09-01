a0f71d05a6eec8c53e18c04e0b7ac4b3e4f1a738
Paul Shapiro <pshapiro138@gmail.com>
2017-08-31 20:05:51 -0700

    WalletDetails: send/receive button copy clarified - tested applying to ContactDetails but Request button was not wide enough for 'Request From'… may need design solution

--- Modules/Wallets/Views/WalletDetailsActionButtonsCell.swift
+++ Modules/Wallets/Views/WalletDetailsActionButtonsCell.swift
@@ -41,7 +41,7 @@ extension WalletDetails
 					let iconImage = UIImage(named: "actionButton_iconImage__send")!
 					let view = UICommonComponents.ActionButton(pushButtonType: .utility, isLeftOfTwoButtons: true, iconImage: iconImage)
 					view.addTarget(self, action: #selector(send_tapped), for: .touchUpInside)
-					view.setTitle(NSLocalizedString("Send", comment: ""), for: .normal)
+					view.setTitle(NSLocalizedString("Send From", comment: ""), for: .normal)
 					self.send_actionButtonView = view
 					self.addSubview(view)
 				}
@@ -49,7 +49,7 @@ extension WalletDetails
 					let iconImage = UIImage(named: "actionButton_iconImage__request")! // borrowing the 'request' image for this
 					let view = UICommonComponents.ActionButton(pushButtonType: .utility, isLeftOfTwoButtons: false, iconImage: iconImage)
 					view.addTarget(self, action: #selector(receive_tapped), for: .touchUpInside)
-					view.setTitle(NSLocalizedString("Receive", comment: ""), for: .normal)
+					view.setTitle(NSLocalizedString("Receive To", comment: ""), for: .normal)
 					self.receive_actionButtonView = view
 					self.addSubview(view)
 				}
